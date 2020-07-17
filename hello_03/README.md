074c47cdfc2bf61358fc0dcd6be619c05cc349d2
hjh0915 <623299157@qq.com>
2020-07-17 12:12:47 +0800

    go文件代码分离

---
+++ hello_03/README.md
@@ -0,0 +1,8 @@
+文件分离代码
+==========
+hello.go 和 utils.go 同属于 package main  
+同一个包内，直接进行调用函数
+
+执行
+===
+> $ go run *.go
---
+++ hello_03/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x = sum(2, 3)
+	fmt.Printf("Hello sum: %d\n", x)
+}
